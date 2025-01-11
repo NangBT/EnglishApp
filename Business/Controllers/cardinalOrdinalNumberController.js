981292a855bcdd6284cf672e6580ed76bb38e907
ricebee2024 <ricebee2024@gmail.com>
2025-01-11 15:25:07 +0700

    #15 - feat: Add Random Date table and Validation Date

--- Business/Controllers/cardinalOrdinalNumberController.js
+++ Business/Controllers/cardinalOrdinalNumberController.js
@@ -1,144 +1,273 @@
+/* #region  Load Data When Page Load */
 $(document).ready(function () {
-  bindingData();
+  showItems();
 
-  $("#randomNumberBtn").click(function () {
-    bindingData();
+  $("#typeSelect").change(function () {
+    showItems();
   });
 
-  $("#checkResultBtn").click(function () {
-    validationNumber();
+  $("#completedBtn").click(function () {
+    var type = $('#typeSelect').val();
+    if (type === randomType.number) {
+      validationNumber();
+    }
+    else if (type === randomType.date) {
+      validationDate();
+    }
+    $('.actionRight').removeClass('hide');
   });
 });
+/* #endregion */
 
-function bindingData() {
+/* #region  Random Data By Type: Random Number, Random Date, Random Time*/
+function showItems() {
+  var type = $('#typeSelect').val();
+  if (type === randomType.number) {
+    loadRandomNumber();
+  }
+  else if (type === randomType.date) {
+    loadRandomDate();
+  }
+  $('.actionRight').addClass('hide');
+}
+/* #endregion */
+
+/* #region  Load Random Number For Cells Label */
+function loadRandomNumber() {
+  $('#firstTh').html('Number');
+  $('#secondTh').html('Cardinal Number');
+  $('#thirdTh').html('Ordinal Number');
+  $('#fourTh').html('Acronym');
+  $('.fourthColumn').removeClass('hide')
+
   var rows = getRows();
   var lengthOfRows = getLengthOfRows(rows);
+
   for (var i = 1; i < lengthOfRows; i++) {
     var row = rows.eq(i);
 
-    var num = utilities.randomNumber();
-    var lbl = row.find("td.numberCol .lbl");
-    lbl.html(utilities.formatNumber(num));
+    var randomValue = utilities.random.number();
+    var lbl = row.find("td.firstColumn .lbl");
+    lbl.html(utilities.format.numberToText(randomValue));
 
-    var hdf = row.find("td.numberCol .hdf");
-    hdf.val(num);
-    // var txt = row.find('td.numberCol .txt');
-    // txt.val(utilities.formatNumber(utilities.randomNumber()));
+    var hdf = row.find("td.firstColumn .hdf");
+    hdf.val(randomValue);
+    // var txt = row.find('td.firstColumn .txt');
+    // txt.val(utilities.format.numberToText(randomValue));
     resetRow(row);
   }
 }
+/* #endregion */
 
-function resetRow(row) {
-  var cardinalNumberCol = row.find("td.cardinalNumberCol");
-  var cardinalNumberTxt = cardinalNumberCol.find(".txt");
-  cardinalNumberTxt.val("");
+/* #region  Load Random Date For Cells Label */
+function loadRandomDate() {
+  var rows = getRows();
+  var lengthOfRows = getLengthOfRows(rows);
 
-  var cardinalNumberLblError = cardinalNumberCol.find(".lblError");
-  cardinalNumberLblError.addClass("hide");
-  cardinalNumberLblError.html("");
+  for (var i = 1; i < lengthOfRows; i++) {
+    var row = rows.eq(i);
 
-  var ordinalNumberCol = row.find("td.ordinalNumberCol");
-  var ordinalNumberTxt = ordinalNumberCol.find(".txt");
-  ordinalNumberTxt.val("");
+    var randomValue = utilities.random.date();
+    var lbl = row.find("td.firstColumn .lbl");
+    lbl.html(moment(randomValue).format('d - DD/MM/YYYY'));
 
-  var ordinalNumberLblError = ordinalNumberCol.find(".lblError");
-  ordinalNumberLblError.addClass("hide");
-  ordinalNumberLblError.html("");
+    var hdf = row.find("td.firstColumn .hdf");
+    hdf.val(randomValue);
+    // var txt = row.find('td.firstColumn .txt');
+    // txt.val(utilities.format.dateToText(randomValue));
+    resetRow(row);
+  }
 
-  var acronymCol = row.find("td.acronymCol");
-  var acronymTxt = acronymCol.find(".txt");
-  acronymTxt.val("");
-
-  var acronymLblError = acronymCol.find(".lblError");
-  acronymLblError.addClass("hide");
-  acronymLblError.html("");
-
-  row.removeClass("rowSuccess");
-  row.removeClass("rowError");
+  $('#firstTh').html('Date');
+  $('#secondTh').html('Full Date (Write)');
+  $('#thirdTh').html('Full Date (Read)');
+  $('#fourTh').html('');
+  $('.fourthColumn').addClass('hide')
 }
+/* #endregion */
 
+/* #region  Reset Value Of Cells in Row */
+function resetRow(row) {
+  var secondColumn = row.find("td.secondColumn");
+  secondColumn.removeClass('successCell');
+  secondColumn.removeClass('errorCell');
+  var secondTxt = secondColumn.find(".txt");
+  secondTxt.val("");
+
+  var secondLblError = secondColumn.find(".lblError");
+  secondLblError.addClass("hide");
+  secondLblError.html("");
+
+  var thirdColumn = row.find("td.thirdColumn");
+  thirdColumn.removeClass('successCell');
+  thirdColumn.removeClass('errorCell');
+  var thirdTxt = thirdColumn.find(".txt");
+  thirdTxt.val("");
+
+  var thirdLblError = thirdColumn.find(".lblError");
+  thirdLblError.addClass("hide");
+  thirdLblError.html("");
+
+  var fourthColumn = row.find("td.fourthColumn");
+  var fourthTxt = fourthColumn.find(".txt");
+  fourthTxt.val("");
+
+  var fourthLblError = fourthColumn.find(".lblError");
+  fourthLblError.addClass("hide");
+  fourthLblError.html("");
+
+  row.removeClass("successCell");
+  row.removeClass("errorCell");
+}
+/* #endregion */
+
+/* #region  Validation Number */
 function validationNumber() {
   var rows = getRows();
   var lengthOfRows = getLengthOfRows(rows);
+  var scores = 0;
   for (var i = 1; i < lengthOfRows; i++) {
     var row = rows.eq(i);
-    // var numberValue = row.find('td.numberCol .lbl').html();
-    //var numberValue = row.find('td.numberCol .txt').val();
+    // var numberValue = row.find('td.firstColumn .lbl').html();
+    //var numberValue = row.find('td.firstColumn .txt').val();
 
-    var hdf = row.find("td.numberCol .hdf");
+    var hdf = row.find("td.firstColumn .hdf");
     var numberValue = hdf.val();
 
-    row.removeClass("rowSuccess");
-    row.removeClass("rowError");
+    row.removeClass("successCell");
+    row.removeClass("errorCell");
 
-    var isCardinalNumber = isValidCell(
+    scores += getTotalCellCorrect(
       numberValue,
       row,
-      "cardinalNumberCol",
+      "secondColumn",
       cardinalOrdinalNumberConstant.type.cardinal
     );
-    var isOrdinalNumber = isValidCell(
+    scores += getTotalCellCorrect(
       numberValue,
       row,
-      "ordinalNumberCol",
+      "thirdColumn",
       cardinalOrdinalNumberConstant.type.ordinal
     );
-    var isAcronym = isValidCell(
+    scores += getTotalCellCorrect(
       numberValue,
       row,
-      "acronymCol",
+      "fourthColumn",
       cardinalOrdinalNumberConstant.type.ordinal
     );
-
-    if (isCardinalNumber && isOrdinalNumber && isAcronym) {
-      row.addClass("rowSuccess");
-    } else {
-      row.addClass("rowError");
-    }
   }
+  $('#scoreLbl').html(scores + "/30");
+  $('.actionRight').removeClass('hide');
 }
 
-function isValidCell(numberValue, row, classCell, type) {
+function getTotalCellCorrect(numberValue, row, classCell, type) {
+  var scores = 0;
   var td = row.find("td." + classCell);
   var txt = td.find(".txt");
 
   var lblError = td.find(".lblError");
   lblError.addClass("hide");
-
+  lblError.removeClass('error')
   if (txt.val().length > 0) {
     var numberText = "";
     if (type === cardinalOrdinalNumberConstant.type.cardinal) {
-      numberText = cardinalOrdinalNumberService.convertNumberToText(parseInt(numberValue), type
-      );
+      numberText = cardinalOrdinalNumberService.convertNumberToText(parseInt(numberValue)).trim();
     } else {
-      numberText = cardinalOrdinalNumberService.convertOrdinalNumberToText(parseInt(numberValue));
+      numberText = cardinalOrdinalNumberService.convertThreeDigitToOrdinalNumberText(parseInt(numberValue)).trim();
     }
 
-    if (classCell === "acronymCol") {
+    if (classCell === "fourthColumn") {
       if (numberText !== "") {
         var twoCharacterLast = numberText.substring(
           numberText.length - 2,
           numberText.length
         );
-        var acronymText = utilities.formatNumber(numberValue) + twoCharacterLast.toLowerCase();
-        if (txt.val().toLowerCase() === acronymText) {
-          return true;
+        var acronymText = utilities.format.numberToText(numberValue) + twoCharacterLast.toLowerCase().trim();
+        if (txt.val().toLowerCase().trim() === acronymText) {
+          td.addClass('successCell');
+          scores++;
         } else {
           numberText = acronymText;
+          lblError.html(numberText);
+          lblError.removeClass("hide");
+          lblError.addClass('error');
+          td.addClass('errorCell');
         }
       }
     } else {
-      if (numberText.trim().toLowerCase() === txt.val().toLowerCase()) {
-        return true;
+      if (numberText.toLowerCase() === txt.val().toLowerCase()) {
+        td.addClass('successCell');
+        scores++;
+      }
+      else {
+        lblError.html(numberText);
+        lblError.removeClass("hide");
+        lblError.addClass('error');
+        td.addClass('errorCell');
       }
     }
-    lblError.html(numberText);
-    lblError.removeClass("hide");
   }
-  return false;
+  return scores;
 }
+/* #endregion */
 
-// - - - Begin Rows - - -
+/* #region  Validation Date */
+function validationDate() {
+  var rows = getRows();
+  var lengthOfRows = getLengthOfRows(rows);
+  var score = 0;
+  for (var i = 1; i < lengthOfRows; i++) {
+    var row = rows.eq(i);
+
+    var firstColumn = row.find('td.firstColumn');
+    var dateText = firstColumn.find('.hdf').val();
+    var dateValue = moment(dateText)
+
+    var secondColumn = row.find('td.secondColumn');
+    secondColumn.removeClass('successCell');
+    secondColumn.removeClass('errorCell');
+    var secondLblError = secondColumn.find('.lblError');
+    secondLblError.addClass('hide');
+    secondLblError.removeClass('error');
+
+    var dateWriteValid = utilities.format.dateToText(dateValue).trim();
+    if (secondColumn.find('.txt').val().trim() === dateWriteValid) {
+      secondColumn.addClass('successCell');
+      score++;
+    }
+    else {
+      secondLblError.html(dateWriteValid);
+      secondLblError.removeClass('hide');
+      secondLblError.addClass('error');
+      secondColumn.addClass('errorCell');
+    }
+
+    var thirdColumn = row.find('td.thirdColumn');
+    thirdColumn.removeClass('errorCell');
+    thirdColumn.removeClass('successCell');
+
+    var thirdLblError = thirdColumn.find('.lblError');
+    thirdLblError.addClass('hide');
+    thirdLblError.removeClass('error');
+
+    var dateReadValid = cardinalOrdinalNumberService.convertDateToFullDateForRead(dateValue).trim();
+    if (thirdColumn.find('.txt').val().trim() === dateReadValid) {
+      thirdColumn.addClass('successCell');
+      score++;
+    }
+    else {
+      thirdLblError.html(dateReadValid);
+      thirdLblError.removeClass('hide');
+      thirdLblError.addClass('error');
+      thirdColumn.addClass('errorCell');
+    }
+  }
+  $('#scoreLbl').html(score + "/20");
+  $('.actionRight').removeClass('hide');
+}
+/* #endregion */
+
+/* #region  Get Length Of Rows */
 function getLengthOfRows(rows) {
   if (rows != null && rows != typeof undefined) {
     return getRows().length;
@@ -146,8 +275,10 @@ function getLengthOfRows(rows) {
   return 0;
   //return 2;
 }
+/* #endregion */
 
+/* #region  Get Rows In Table */
 function getRows() {
   return $("#randomNumberTbl").find("tr");
 }
-// - - - End Rows - - -
+/* #endregion */
